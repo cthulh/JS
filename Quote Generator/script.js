b48a6ef4cfe6e1e67cf4e6baf6661f1ab7101681
cthulh <tryglavson@hotmail.co.uk>
2016-06-03 21:37:12 +0100

    Final changes to style and js

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,10 +1,12 @@
 //Array of a 100 quotes = quotes_array
+var get_quote = function(q) {
+	var rand = function(limit) {
+		return Math.floor((Math.random() * limit));
+	}
+	var x = rand(99);;
+	$('#quote').html(q[x][0]);
+	$('#movie').html(q[x][1]);
 
-var rand = function(limit) {
-	return Math.floor((Math.random() * limit));
-}
-var x = rand(99);
+};
 
-$('#quote').html(quotes_array[x][0]);
-$('#movie').html(quotes_array[x][1]);
-
+$(document).ready(get_quote(quotes_array));
