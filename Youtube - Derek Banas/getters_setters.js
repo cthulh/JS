020f96f379bbd78252fda856258c29bf65a8013f
cthulh <tryglavson@hotmail.co.uk>
2016-08-28 23:28:48 +0100

    OO concepts briefly

---
+++ Youtube - Derek Banas/getters_setters.js
@@ -0,0 +1,83 @@
+var address = {
+  street: "No street",
+  city: "No city",
+  state: "No state",
+  
+  get getAddress(){
+    return this.street + ", " + this.city + ", " + this.state;
+  },
+  
+  set setAddress(theAddress){
+    var parts = theAddress.toString().split(", ");
+    this.street = parts[0] || "";
+    this.city = parts[1] || "";
+    this.state = parts[2] || "";
+  }
+}
+
+address.setAddress = "123 main st, Pittsburgh, PA";
+
+document.write("Address : " + address.getAddress + "<br>");
+document.write("City : " + address.city + "<br>");
+
+// depricated version
+
+function Coordinates(){
+  this.latitude = 0;
+  this.longtitude = 0;
+}
+
+Object.__defineGetter__.call(Coordinates.prototype, "getCoords", function(){
+  return "Lat : " + this.latitude + ", Long : " + this.longtitude;
+});
+Object.__defineSetter__.call(Coordinates.prototype, "setCoords", function(coords){
+  var parts = coords.toString().split(", ");
+  this.latitude = parts[0] || "";
+  this.longtitude = parts[1] || "";
+});
+
+var testCoords = new Coordinates;
+testCoords.setCoords = "40.22, 78.55";
+document.write("Coordinates : " + testCoords.getCoords + "<br>");
+
+// define property version
+
+function Point(){
+  this.xPos = 0;
+  this.yPos = 0;
+}
+
+Object.defineProperty(Point.prototype, "pointPosition", {
+  get: function(){
+    return "X : " + this.xPos + ", Y : " + this.yPos;
+  },
+  set: function(thePoint){
+    var parts = thePoint.toString().split(", ");
+    this.xPos = parts[0] || "";
+    this.yPos = parts[1] || "";
+  }
+});
+
+var aPoint = new Point();
+
+aPoint.pointPosition = "100, 200";
+document.write("Point Position : " + aPoint.pointPosition + "<br>");
+
+// ECAM 5.1 version
+
+var Circle = function(radius) {
+  this._radius = radius;
+}
+
+Circle.prototype = {
+  set radius(radius) {this._radius = radius;},
+  get radius() {return this._radius;},
+  get area() { return Math.PI * (this._radius * this._radius);}
+};
+
+var circ = new Circle(10);
+
+circ.radius = 15;
+
+document.write("A circle with radius " + circ.radius + " has an area " + circ.area.toFixed(2) + "<br>");
+
--- Youtube - Derek Banas/objects.js
+++ Youtube - Derek Banas/objects.js
@@ -26,3 +26,55 @@ Person.prototype.sayHi = function () {
 }
 
 document.write(bobSmith.sayHi() + "<br>");
+
+for (var prop in bobSmith){
+  document.write(prop + " : " + bobSmith[prop] + "<br>");
+}
+
+document.write("name Property of bobSmith : " + bobSmith.hasOwnProperty("name") + "<br>");
+document.write("sayHi Property of bobSmith : " + bobSmith.hasOwnProperty("sayHi") + "<br>");
+
+//extend Array prototype
+
+Array.prototype.inArray = function (value) {
+  for (i = 0; i< this.length; i = i +1){
+    if(this[i] === value){
+      return true;
+    }
+  }
+  return false;
+}
+
+var sampArray = [1,2,3,4,5];
+
+document.write("13 in sampArray?: " + sampArray.inArray(13) + "<br>");
+
+
+// private properties
+
+function SecretCode() {
+  var secretNum = 78;
+  
+  this.guessNum = function(num) {
+    if(num > secretNum) {
+      return "Go lower!";
+    } else if (num < secretNum) {
+      return "Go higher!"
+    } else {
+      return "You guessed it!"
+    }
+  }
+}
+
+var secret = new SecretCode();
+document.write("Value of secretNum : " + secret.secretNum + "<br>");
+document.write("Is 70 the secret number? : " + secret.guessNum(70) + "<br>");
+document.write("Is 10 the secret number? : " + secret.guessNum(10) + "<br>");
+document.write("Is 700 the secret number? : " + secret.guessNum(700) + "<br>");
+document.write("Is 78 the secret number? : " + secret.guessNum(78) + "<br>");
+
+SecretCode.prototype.getSecret = function() {
+  return this.secretNum;
+}
+
+document.write("Secret number is : " + secret.getSecret() + "<br>");
