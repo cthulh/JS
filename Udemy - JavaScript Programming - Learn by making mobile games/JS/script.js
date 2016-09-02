9e88bf2a7aff2ae435476cd6969dc179204f499b
cthulh <tryglavson@hotmail.co.uk>
2016-09-02 22:45:42 +0100

    Changing colors of rectangle based on coordinates

--- Udemy - JavaScript Programming - Learn by making mobile games/JS/script.js
+++ Udemy - JavaScript Programming - Learn by making mobile games/JS/script.js
@@ -1,17 +1,41 @@
-// starting position
+// starting position & width and height of rectangle
 var x = 10, y = 10, w = 20, h = 30;
 var canvas = document.getElementById("myCanvas");
+// speed of movement of rectangle
 var speed = 2;
 var ctx = canvas.getContext("2d");
+// color zones
+var blueZone, greenZone;
 
 var update = function() {
+  if (x >= 270) {
+    // rectangle reaches the right-hand border
+    x = 270;
+    // reversing direction of the rectangle
+    speed = -speed;
+  } else if (x <= 10) {
+    // rectangle reaches starting position
+    x = 10;
+    // reversing direction again
+    speed = -speed;
+  }
+  // incrementing position of rectangle at each re-draw of canvas
   x += speed; 
-  console.log(x);
+
+  // define color
+  blueZone = x > 0 && x < 100;
+  greenZone = !blueZone && x < 200;
 };
 
 var draw = function() {
   ctx.clearRect(0,0,500,300);
-  ctx.fillStyle = "rgb(200, 0, 100)";
+  if (blueZone) {
+    ctx.fillStyle = "#3333FF";
+  } else if (greenZone) {
+    ctx.fillStyle = "#00CC66";
+  } else {
+    ctx.fillStyle = "rgb(200, 0, 100)";
+  }
   ctx.fillRect(x, y, w, h);
 }
 
