813ed08e39b6430a531aff9aff9c3ff2c67a6894
cthulh <tryglavson@hotmail.co.uk>
2016-09-02 22:29:54 +0100

    Drawing a rectangle on canvas

--- Udemy - JavaScript Programming - Learn by making mobile games/JS/script.js
+++ Udemy - JavaScript Programming - Learn by making mobile games/JS/script.js
@@ -1,1 +1,25 @@
-alert("Hello World");
+// starting position
+var x = 10, y = 10, w = 20, h = 30;
+var canvas = document.getElementById("myCanvas");
+var speed = 2;
+var ctx = canvas.getContext("2d");
+
+var update = function() {
+  x += speed;
+  console.log(x);
+};
+
+var draw = function() {
+  ctx.clearRect(0,0,500,300);
+  ctx.fillStyle = "rgb(200, 0, 100)";
+  ctx.fillRect(x, y, w, h);
+}
+
+var step = function() {
+
+  update();
+  draw();
+
+  window.requestAnimationFrame(step)
+};
+step();
