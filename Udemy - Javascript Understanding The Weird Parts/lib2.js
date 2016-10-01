fb0e3037a24e8c2a79feb67444ba73c033e716ab
cthulh <tryglavson@hotmail.co.uk>
2016-10-01 22:10:46 +0100

    More on objects

--- Udemy - Javascript Understanding The Weird Parts/lib2.js
+++ Udemy - Javascript Understanding The Weird Parts/lib2.js
@@ -13,5 +13,35 @@ person[lastNameProperty] = "Alicea";
 console.log(person);
 console.log("First name = " + person[firstNameProperty]);
 
+// dot operator
+console.log(person.firstName + " " + person.lastName);
+
+person.address = new Object();
+person.address.street = "111 Main St";
+person.address.city = "New York";
+person.address.state = "NY";
+console.log(person.address);
+// computed member
+console.log(person["address"]["city"]);
+
+// object literal syntax
+
+var ironman = {
+  firstName: "Tony",
+  lastName: "Stark"
+};
+
+console.log(ironman);
+
+// objects on the fly
+
+function greet(person) {
+  console.log("Hi " + person.firstName + "!");
+}
+greet({
+  firstName: "Frank",
+  lastName: "Estine"
+});
+greet(ironman);
 
 
